14f8bae535eb340388930608bfb9645500622dc0
Stepan Krimskiy <cruze132@gmail.com>
2016-11-27 16:18:18 +0700

    fix code format

--- src/main/java/com/cruze/client/RestEasyClientGet.java
+++ src/main/java/com/cruze/client/RestEasyClientGet.java
@@ -33,15 +33,15 @@ public class RestEasyClientGet {
     }
 
     private static void doGet() throws Exception {
-            ClientRequest request = new ClientRequest(
-                    "http://localhost:8080/json/product/get");
-            request.accept("application/json");
-            ClientResponse<String> response = request.get(String.class);
+        ClientRequest request = new ClientRequest(
+                "http://localhost:8080/json/product/get");
+        request.accept("application/json");
+        ClientResponse<String> response = request.get(String.class);
 
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatus());
-            }
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatus());
+        }
 
         Gson gson = new Gson();
         Book book = gson.fromJson(response.getEntity(), Book.class);
@@ -51,32 +51,32 @@ public class RestEasyClientGet {
     }
 
     private static void doPost() throws Exception {
-            ClientRequest request = new ClientRequest(
-                    "http://localhost:8080/json/product/post");
-            request.accept("application/json");
+        ClientRequest request = new ClientRequest(
+                "http://localhost:8080/json/product/post");
+        request.accept("application/json");
 
         Book book = new Book();
         book.setAuthor("uncle Bob");
         book.setId(23);
         book.setTitle("cleanCode");
 
-            request.body("application/json", new Gson().toJson(book));
+        request.body("application/json", new Gson().toJson(book));
 
-            ClientResponse<String> response = request.post(String.class);
+        ClientResponse<String> response = request.post(String.class);
 
-            if (response.getStatus() != 201) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatus());
-            }
+        if (response.getStatus() != 201) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatus());
+        }
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    new ByteArrayInputStream(response.getEntity().getBytes())));
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                new ByteArrayInputStream(response.getEntity().getBytes())));
 
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
 
     }
 
